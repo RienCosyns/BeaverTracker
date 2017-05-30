a5984286796a5c71bb391a0ed136525021cd88c4
[rien.cosyns] <[rien.cosyns@gmail.com]>
2017-05-30 12:01:21 +0200

    Empty view script

--- BeaverTrackerProject/viewscript.js
+++ BeaverTrackerProject/viewscript.js
@@ -1,2 +1,30 @@
 // empty viewscript based on the view prompt
 
+var beaverViewer = {
+    displayBeavers: function(){
+        //args : nada
+
+        // code here
+
+        return beaverEvents.displayBeavers();
+    },
+    stringifyBeaver: function(beaverObj){
+        beaverString = "";
+
+        //code here
+
+        return beaverString
+    },
+    addLocationButton: function(){
+        //code here
+    },
+    addTrackButton: function(){
+        //code here
+    },
+    untrackAllButton: function(){
+        //code here
+    },
+    trackAllButton: function(){
+        //code here
+    }
+}
